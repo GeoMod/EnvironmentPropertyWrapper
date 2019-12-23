ba633c61506e5aff93427e31e43f03585f2d20a1
Dan O'Leary <olearydaniel@me.com>
2019-12-23 09:47:42 -0600

    Initial Commit

---
+++ TestView/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestView
+//
+//  Created by Daniel O'Leary on 12/23/19.
+//  Copyright © 2019 Impulse Coupled Development. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
